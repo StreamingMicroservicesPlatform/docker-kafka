015f604ff05f8310e604ca9d91d12937200337d9
Adrian Muraru <amuraru@adobe.com>
2020-03-24 08:40:36 +0200

    Dynamic java heap settings for cruise-control
    Configure CruiseControl Java heap sizes using container available memory ratios.
    This is available in Java11 introduced in 
    (https://github.com/solsson/dockerfiles/commit/c6cfc1cdd3051a4bc8e3a0eb635099e8b32a3ed0#diff-25116d53120aac394f7afcbad1211369)
    
    Using these settings allows the tuning of actual heap size of the CC JVM using docker container limits and not statically 1GB

--- cruise-control/opt/cruise-control/start.sh
+++ cruise-control/opt/cruise-control/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
 cd /opt/cruise-control
+
+# Override heap settings for container environments
+export KAFKA_HEAP_OPTS="-XX:InitialRAMPercentage=30 -XX:MaxRAMPercentage=70 -XX:MinRAMPercentage=80"
+
 /bin/bash ${DEBUG:+-x} /opt/cruise-control/kafka-cruise-control-start.sh /opt/cruise-control/config/cruisecontrol.properties 8090
