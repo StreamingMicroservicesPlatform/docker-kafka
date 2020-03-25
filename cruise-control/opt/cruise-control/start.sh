2dea76d8266dc4db54b147c6ddaca9bd8f9bd170
Adrian Muraru <amuraru@adobe.com>
2020-03-25 16:56:31 +0200

    Update start.sh

--- cruise-control/opt/cruise-control/start.sh
+++ cruise-control/opt/cruise-control/start.sh
@@ -2,7 +2,8 @@
 set -e
 cd /opt/cruise-control
 
-# Override heap settings for container environments
-export KAFKA_HEAP_OPTS="-XX:InitialRAMPercentage=30 -XX:MaxRAMPercentage=70 -XX:MinRAMPercentage=80"
-
+# Set heap memory settings for container environments
+if [ -z "$KAFKA_HEAP_OPTS" ]; then
+  export KAFKA_HEAP_OPTS="-XX:InitialRAMPercentage=30 -XX:MaxRAMPercentage=70 -XX:MinRAMPercentage=80"
+fi
 /bin/bash ${DEBUG:+-x} /opt/cruise-control/kafka-cruise-control-start.sh /opt/cruise-control/config/cruisecontrol.properties 8090
